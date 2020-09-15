e5639de047104292c36c72a9ecb71812915cf73a
Casey The Great <casey@caseypitman.com>
2020-09-15 08:56:22 -0500

    working on setting ratios and flex

--- src/App.js
+++ src/App.js
@@ -7,31 +7,21 @@ function App() {
 
   return (
     <div className="App">
-      <table className = 'board'>
-        <tbody>
-          <tr>
-            <th className = 'title' colSpan = '4'>Memory</th>
-          </tr>
-          <tr>
-            <td className = 'card face-down'></td>
-            <td className = 'card face-down' ></td>
-            <td className = 'card face-down'></td>
-            <td className = 'card face-up'></td>
-          </tr>
-          <tr>
-            <td className = 'card matched'></td>
-            <td className = 'card face-down'></td>
-            <td className = 'card face-down'></td>
-            <td className = 'card face-down'></td>
-          </tr>
-          <tr>
-            <td className = 'card face-down'></td>
-            <td className = 'card matched'></td>
-            <td className = 'card face-down'></td>
-            <td className = 'card face-down'></td>
-          </tr>
-        </tbody>
-      </table>
+      <h1 className = 'title'> Memory </h1>
+      <div className = 'board' ratio = '1x1'>
+        <div className='card'></div>
+        <div className='card'></div>
+        <div className='card'></div>
+        <div className='card'></div>
+        <div className='card'></div>
+        <div className='card'></div>
+        <div className='card'></div>
+        <div className='card'></div>
+        <div className='card'></div>
+        <div className='card'></div>
+        <div className='card'></div>
+        <div className='card'></div>
+      </div>
 
       <div className = 'button-div'>
         <button className = 'button'>Start Over</button>
