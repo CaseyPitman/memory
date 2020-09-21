3c6adcf55994832bb2cdf5c231b3c5c15994816e
Casey The Great <casey@caseypitman.com>
2020-09-21 13:30:08 -0500

    Updates

---
+++ precache-manifest.fa6ac8ce8a33bc60dc89e5790a64a71b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a22bc32c3ca3e813fd6a68b405d135dd",
+    "url": "/memory/index.html"
+  },
+  {
+    "revision": "91c8c336082ba23dc53f",
+    "url": "/memory/static/css/main.3e09d90e.chunk.css"
+  },
+  {
+    "revision": "638414e5368b38a42954",
+    "url": "/memory/static/js/2.ff9c4c18.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/memory/static/js/2.ff9c4c18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91c8c336082ba23dc53f",
+    "url": "/memory/static/js/main.c87ce0df.chunk.js"
+  },
+  {
+    "revision": "62e0eae13c9afaa66ed5",
+    "url": "/memory/static/js/runtime-main.c4984c0b.js"
+  },
+  {
+    "revision": "5d49e223d5d4978fd191a3dbadd08a20",
+    "url": "/memory/static/media/card-back.5d49e223.jpg"
+  },
+  {
+    "revision": "34c882923a990b87c76e446a6e46a142",
+    "url": "/memory/static/media/castle.34c88292.jpg"
+  },
+  {
+    "revision": "4edb5de873e0e2f86521f75aa0111a53",
+    "url": "/memory/static/media/dragon.4edb5de8.jpg"
+  },
+  {
+    "revision": "6c2fc9d50ad4e650efd571e52c09b4a3",
+    "url": "/memory/static/media/key.6c2fc9d5.jpg"
+  },
+  {
+    "revision": "d7c86c552561fd986c26d5ef4b0ad0e9",
+    "url": "/memory/static/media/moon.d7c86c55.jpg"
+  },
+  {
+    "revision": "9278e6092069739a56d6a8890c5b65cc",
+    "url": "/memory/static/media/unicorn.9278e609.jpg"
+  },
+  {
+    "revision": "d3a4376b4877f89e368262d5caf9aff5",
+    "url": "/memory/static/media/wizard.d3a4376b.jpg"
+  }
+]);
